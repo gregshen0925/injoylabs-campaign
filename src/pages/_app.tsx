1014b8c703d955704b90ab6928806886ccbc77ad
GregShen <gregshen0925@gmail.com>
2023-01-02 20:38:30 +0800

    add animation in page transition

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import { trpc } from "../utils/trpc";
 import "../styles/globals.css";
 import { AptosWalletProvider } from "../context/AptosWalletProvider";
 import { Toaster } from "react-hot-toast";
+import { AnimatePresence, motion } from "framer-motion";
+import { useRouter } from "next/router";
 
 const toastOptions = {
   style: {
@@ -27,10 +29,34 @@ const toastOptions = {
 };
 
 const MyApp: AppType = ({ Component, pageProps }) => {
+  const router = useRouter();
   return (
     <AptosWalletProvider>
       <Toaster position="top-center" toastOptions={toastOptions} />
-      <Component {...pageProps} />
+      <AnimatePresence exitBeforeEnter>
+        <motion.div
+          key={router.route}
+          initial="initialState"
+          animate="animateState"
+          exit="exitState"
+          transition={{ duration: 0.75 }}
+          variants={{
+            initialState: {
+              opacity: 0,
+              clipPath: "polygon(0 0,100% 0,100% 100%, 0% 100%",
+            },
+            animateState: {
+              opacity: 1,
+              clipPath: "polygon(0 0,100% 0,100% 100%, 0% 100%",
+            },
+            exitState: {
+              clipPath: "polygon(50% 0,50% 0,50% 100%, 50% 100%",
+            },
+          }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </AptosWalletProvider>
   );
 };
