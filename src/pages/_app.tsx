5d4192ae9d76829e7fa6d798bccb05e4336122ad
GregShen <gregshen0925@gmail.com>
2023-01-05 12:28:42 +0800

    fix animation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -43,14 +43,14 @@ const MyApp: AppType = ({ Component, pageProps }) => {
           variants={{
             initialState: {
               opacity: 0,
-              clipPath: "polygon(0 0,100% 0,100% 100%, 0% 100%",
+              clipPath: "polygon(50% 0,50% 0,50% 100%, 50% 100%",
             },
             animateState: {
               opacity: 1,
-              clipPath: "polygon(0 0,100% 0,100% 100%, 0% 100%",
+              clipPath: "polygon(0% 0,100% 0,100% 100%, 0% 100%",
             },
             exitState: {
-              clipPath: "polygon(50% 0,50% 0,50% 100%, 50% 100%",
+              clipPath: "polygon(0 0,100% 0,100% 100%, 0% 100%",
             },
           }}
         >
